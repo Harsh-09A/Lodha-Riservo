e1eed5e720f956d2adc477ee22dde37fd4d172de
Harsh Athavale <harsh.autowebbed@gmail.com>
2023-11-20 17:47:55 +0530

    Website Done

--- riservo/js/script.js
+++ riservo/js/script.js
@@ -1,3 +1,21 @@
 Fancybox.bind('[data-fancybox="gallery-a"]', {
   // Your custom options
 });
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  autoplay: true,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 4,
+    },
+  },
+});
